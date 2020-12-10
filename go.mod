74fcdecbb0ba581e7064ac40f586b7c1ea3a0c0c
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-10 16:46:45 +0900

    urfave/cli を導入

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/aws/aws-sdk-go v1.36.4
 	github.com/dustin/go-humanize v1.0.0
 	github.com/ktr0731/go-fuzzyfinder v0.2.1
+	github.com/urfave/cli/v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/kou-pg-0131/s3ls/src/cmd"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	if err := cmd.New().Do(); err != nil {
-		panic(err)
+	app := cli.NewApp()
+	app.Name = "s3ls"
+	app.Usage = "usage"          // TODO
+	app.UsageText = "usage text" // TODO
+	app.HideHelpCommand = true
+
+	app.Action = func(ctx *cli.Context) error {
+		return cmd.New().Do()
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Printf("error: %s\n", err)
+		os.Exit(1)
 	}
 }
