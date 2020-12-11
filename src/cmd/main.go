b6b1e10b19bc548014a8a57d80d16a0fc3234991
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-11 21:18:32 +0900

    S3Controller.FindObject() を実装

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -3,12 +3,10 @@ package cmd
 import (
 	"bytes"
 	"fmt"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/dustin/go-humanize"
 	"github.com/kou-pg-0131/s3fzf/src/infrastructures"
 	"github.com/kou-pg-0131/s3fzf/src/interfaces/controllers"
 	"github.com/kou-pg-0131/s3fzf/src/interfaces/gateways"
@@ -39,7 +37,7 @@ func (c *Command) Do() error {
 		return err
 	}
 
-	o, err := c.findObject(*b.Name)
+	o, err := c.s3Controller.FindObject(*b.Name)
 	if err != nil {
 		return err
 	}
@@ -55,66 +53,4 @@ func (c *Command) Do() error {
 
 	fmt.Print(buf.String())
 	return nil
-}
-
-func (c *Command) findObject(bucket string) (*s3.Object, error) {
-	os := []*s3.Object{}
-
-	chlserr := make(chan error, 1)
-	chfderr := make(chan error, 1)
-	chidx := make(chan int, 1)
-
-	go func() {
-		resp, err := c.s3Client.GetBucketLocation(bucket)
-		if err != nil {
-			chlserr <- err
-		}
-
-		c.s3Client.SetAPI(s3.New(session.New(), aws.NewConfig().WithRegion(*resp.LocationConstraint)))
-
-		tkn := (*string)(nil)
-		for {
-			resp, err := c.s3Client.ListObjects(bucket, tkn)
-			if err != nil {
-				chlserr <- err
-			}
-
-			os = append(os, resp.Contents...)
-			if len(os) == 0 {
-				chlserr <- fmt.Errorf("`s3://%s` is empty", bucket)
-			}
-
-			tkn = resp.NextContinuationToken
-			if tkn == nil {
-				break
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	go func() {
-		i, err := c.fzf.Find(&os, func(i int) string {
-			return *os[i].Key
-		}, func(i, w, h int) string {
-			if i == -1 {
-				return ""
-			}
-
-			return fmt.Sprintf("%s\n\nSize: %s\nLastModified: %s", *os[i].Key, humanize.Bytes(uint64(*os[i].Size)), *os[i].LastModified)
-		})
-		if err != nil {
-			chfderr <- err
-		}
-		chidx <- i
-	}()
-
-	select {
-	case err := <-chlserr:
-		c.fzf.Close()
-		return nil, err
-	case err := <-chfderr:
-		return nil, err
-	case idx := <-chidx:
-		return os[idx], nil
-	}
 }
--- src/interfaces/controllers/s3_controller.go
+++ src/interfaces/controllers/s3_controller.go
@@ -2,14 +2,19 @@ package controllers
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/dustin/go-humanize"
 	"github.com/kou-pg-0131/s3fzf/src/interfaces/gateways"
 )
 
 // IS3Controller .
 type IS3Controller interface {
 	FindBucket() (*s3.Bucket, error)
+	FindObject(bucket string) (*s3.Object, error)
 }
 
 // S3Controller .
@@ -73,5 +78,69 @@ func (c *S3Controller) FindBucket() (*s3.Bucket, error) {
 		return nil, err
 	case idx := <-chidx:
 		return bs[idx], nil
+	}
+}
+
+// FindObject .
+func (c *S3Controller) FindObject(bucket string) (*s3.Object, error) {
+	os := []*s3.Object{}
+
+	chlserr := make(chan error, 1)
+
+	go func() {
+		resp, err := c.s3Client.GetBucketLocation(bucket)
+		if err != nil {
+			chlserr <- err
+		}
+
+		c.s3Client.SetAPI(s3.New(session.New(), aws.NewConfig().WithRegion(*resp.LocationConstraint)))
+
+		tkn := (*string)(nil)
+		for {
+			resp, err := c.s3Client.ListObjects(bucket, tkn)
+			if err != nil {
+				chlserr <- err
+			}
+
+			os = append(os, resp.Contents...)
+			if len(os) == 0 {
+				chlserr <- fmt.Errorf("`s3://%s` is empty", bucket)
+			}
+
+			tkn = resp.NextContinuationToken
+			if tkn == nil {
+				break
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	chfderr := make(chan error, 1)
+	chidx := make(chan int, 1)
+
+	go func() {
+		i, err := c.fzf.Find(&os, func(i int) string {
+			return *os[i].Key
+		}, func(i, w, h int) string {
+			if i == -1 {
+				return ""
+			}
+
+			return fmt.Sprintf("%s\n\nSize: %s\nLastModified: %s", *os[i].Key, humanize.Bytes(uint64(*os[i].Size)), *os[i].LastModified)
+		})
+		if err != nil {
+			chfderr <- err
+		}
+		chidx <- i
+	}()
+
+	select {
+	case err := <-chlserr:
+		c.fzf.Close()
+		return nil, err
+	case err := <-chfderr:
+		return nil, err
+	case idx := <-chidx:
+		return os[idx], nil
 	}
 }
