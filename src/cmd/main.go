8c56a2f44c9c245ffda8aecab975372066ce9560
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-11 21:24:01 +0900

    不要なフィールドを削除

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -4,29 +4,18 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/kou-pg-0131/s3fzf/src/infrastructures"
 	"github.com/kou-pg-0131/s3fzf/src/interfaces/controllers"
-	"github.com/kou-pg-0131/s3fzf/src/interfaces/gateways"
 )
 
 // Command .
 type Command struct {
 	s3Controller controllers.IS3Controller
-	s3Client     gateways.IS3Client
-	fzf          gateways.IFZF
 }
 
 // New .
 func New() *Command {
-	s3api := s3.New(session.New(), aws.NewConfig().WithRegion("us-east-1"))
-
 	return &Command{
 		s3Controller: controllers.NewS3ControllerFactory().Create(),
-		s3Client:     infrastructures.NewS3Client(s3api),
-		fzf:          infrastructures.NewFZF(),
 	}
 }
 
