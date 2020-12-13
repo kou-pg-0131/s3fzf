3f31c9de55692a82d1eb62d53376fd89b8bfce81
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-12-13 11:01:02 +0900

    削除機能を実装

--- src/cmd/rm.go
+++ src/cmd/rm.go
@@ -1,6 +1,36 @@
 package cmd
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Remove .
 func (c *Command) Remove(bucket string) error {
+	if bucket == "" {
+		b, err := c.s3Controller.FindBucket()
+		if err != nil {
+			return err
+		}
+		bucket = *b.Name
+	}
+
+	o, err := c.s3Controller.FindObject(bucket)
+	if err != nil {
+		return err
+	}
+
+	yes, err := c.confirmer.Confirm(fmt.Sprintf("remove `s3://%s/%s`?(y/N): ", bucket, *o.Key))
+	if err != nil {
+		return err
+	}
+	if !yes {
+		return errors.New("cancelled")
+	}
+
+	if err := c.s3Controller.DeleteObject(bucket, *o.Key); err != nil {
+		return err
+	}
+
 	return nil
 }
